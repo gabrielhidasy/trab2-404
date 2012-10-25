304d526e5073dcee19dfc01a80e1cda14ad06dc9
Gabriel Hidasy Rezende <ra116928@arm1.lab.ic.unicamp.br>
2012-10-25 16:07:04 -0200

    Criadas funções de ler long octa e long hexa

--- myscanf.s
+++ myscanf.s
@@ -161,7 +161,8 @@ _le_int_loop_out:
 	rsbne	r6, r6, #0
 	str	r6, [r5]
 	ldmfd 	sp!, {R4 - R11, pc}
-_le_lint:	
+_le_lint:
+	mov 	pc, lr
 _le_hexa:
 	stmfd	sp!, {R4-R11, lr}
 	@load the next pointer
@@ -193,7 +194,41 @@ _le_hexa_loop
 	@and do the 4 - bitshift to left
 	mov	r6, r6, lsl #4
 	b 	_le_hexa_loop
-_le_lhexa:	
+_le_lhexa:
+	stmfd	sp!, {R4-R11, lr}
+	@load the next pointer
+	ldr	r5, [r11], #4
+	@acummulator = r1:r0 = 0
+	mov 	r1, #0
+	mov	r0, #0
+_le_lhexa_loop
+	@get character from input buffer
+	ldrb	r4, [r2]
+        @---------------------------------------
+	@if its an space or \n its the end
+	cmp	r4, #' '
+	streq	r1, [r5], #1
+	streq	r0, [r5]
+	ldmeqfd 	sp!, {R4 - R11, pc}
+	cmp	r0, #'\n
+	streq	r1, [r5], #1
+	streq	r0, [r5]'
+	@--------------------------------------
+	@next char
+	add 	r2, r2, #1
+	@transforms in number
+	bl 	chartonumber
+	@if its less than zero or greater then F
+	@its not valid, error
+	cmp	r4, #0
+	blt	_myscanf_real_error
+	cmp	r4, #15
+	bgt	_myscanf_real_error
+	@else add it to accumulator
+	add 	r0, r0, r4
+	@and do the 4 - bitshift to left
+	bl 	long4lsl
+	b 	_le_lhexa_loop
 _le_octa:
 	stmfd	sp!, {R4-R11, lr}
 	@load the next pointer
@@ -225,7 +260,42 @@ _le_octa_loop
 	@and do the 3 - bitshift to left
 	mov	r6, r6, lsl #3
 	b 	_le_octa_loop
-_le_locta:	
+_le_locta:
+	_le_lhexa:
+	stmfd	sp!, {R4-R11, lr}
+	@load the next pointer
+	ldr	r5, [r11], #4
+	@acummulator = r1:r0 = 0
+	mov 	r1, #0
+	mov	r0, #0
+_le_locta_loop
+	@get character from input buffer
+	ldrb	r4, [r2]
+        @---------------------------------------
+	@if its an space or \n its the end
+	cmp	r4, #' '
+	streq	r1, [r5], #1
+	streq	r0, [r5]
+	ldmeqfd 	sp!, {R4 - R11, pc}
+	cmp	r0, #'\n
+	streq	r1, [r5], #1
+	streq	r0, [r5]'
+	@--------------------------------------
+	@next char
+	add 	r2, r2, #1
+	@transforms in number
+	bl 	chartonumber
+	@if its less than zero or greater then 7
+	@its not valid, error
+	cmp	r4, #0
+	blt	_myscanf_real_error
+	cmp	r4, #7
+	bgt	_myscanf_real_error
+	@else add it to accumulator
+	add 	r0, r0, r4
+	@and do the 3 - bitshift to left
+	bl 	long3lsl
+	b 	_le_locta_loop
 _le_uint:
 	stmfd	sp!, {R4-R11, lr}
 	@load the next pointer
